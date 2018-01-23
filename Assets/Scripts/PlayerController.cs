bca4b6b2e444d853bd133c12ae013b7746101a55
lulyz419 <lulyz419@hotmail.com>
2018-01-23 18:48:34 +0100

    hola luly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D myRigidBody;
     public float playerJumpForce = 500f;
+    private Animator myAnim;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
-		
-	}
+        myAnim = GetComponent<Animator>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,6 +21,16 @@ public class PlayerController : MonoBehaviour {
         {
             myRigidBody.AddForce(transform.up * playerJumpForce);
         }
-		
+
+        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            SceneManager.LoadScene("runner");
+        }
+        
+    }
 }
