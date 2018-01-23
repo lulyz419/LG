68d17a0f8cad91f7477b1f333c70754bab081277
lulyz419 <lulyz419@hotmail.com>
2018-01-23 11:18:19 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D myRigidBody;
+    public float playerJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRigidBody.AddForce(transform.up * playerJumpForce);
+        }
+		
+	}
+}
