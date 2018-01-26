b1e284f2f53b897a787e0c3414f28c718c1fffb7
lulyz419 <lulyz419@hotmail.com>
2018-01-26 09:01:05 +0100

    viernes 26
    hola luly qué tal estás, espero que bien. Te subo los últimos cambios del vidijuego, que pases un buen fin de semana

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,35 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D myRigidBody;
     public float playerJumpForce = 500f;
     private Animator myAnim;
+    private float deathTime = -1;
+    private Collider2D myCollider;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 2;
+
 
     // Use this for initialization
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonUp("Jump"))
-        {
-            myRigidBody.AddForce(transform.up * playerJumpForce);
-        }
 
-        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (deathTime == -1)
+        {
+
+            if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
+            {
+
+                if (myRigidBody.velocity.y < 0)
+                {
+                    myRigidBody.velocity = Vector2.zero;
+                }
+                else
+                {
+                    myRigidBody.AddForce(transform.up * playerJumpForce * 0.75f);
+                }
+
+                if (jumpsLeft == 1)
+                {
+                    myRigidBody.AddForce(transform.up * playerJumpForce);
+                }
+
+                myRigidBody.AddForce(transform.up * playerJumpForce);
+                jumpsLeft--;
+            }
+
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if (Time.time > deathTime + 2)
+            {
+                SceneManager.LoadScene("runner");
+            }
+
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            SceneManager.LoadScene("runner");
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>() )
+            {
+                moveLefter.enabled = false;
+            }
+
+            deathTime = Time.time;
+            myAnim.SetBool("death", true);
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(transform.up * playerJumpForce);
+            myCollider.enabled = false;
         }
-        
+
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsLeft = 2;
+        }
+
+
     }
 }
--- Assets/Scripts/PrefabSpawner.cs
+++ Assets/Scripts/PrefabSpawner.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PrefabSpawner : MonoBehaviour {
     private float nextSpawn = 0;
     public Transform prefabToSpawn;
-    public float spawnRate = 1;
-    public float randomDelay = 1;
-
+    public AnimationCurve spawnCurve;
+    public float curveLenghtInSeconds = 30f;
+    private float startTime;
+    public float jitter = 0.25f;
 
 	// Use this for initialization
 	void Start () {
+        startTime = Time.time;
 		
 	}
 	
@@ -19,7 +21,18 @@ public class PrefabSpawner : MonoBehaviour {
 	  if (Time.time > nextSpawn)
         {
             Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
-            nextSpawn = Time.time + spawnRate + Random.Range (0, randomDelay);
+            //nextSpawn = Time.time + spawnRate + Random.Range (0, randomDelay);
+
+            float curvePos = (Time.time - startTime) / curveLenghtInSeconds;
+            if (curvePos > 1f)
+            {
+                curvePos = 1f;
+                startTime = Time.time;
+            }
+
+            nextSpawn = Time.time + spawnCurve.Evaluate(curvePos) + Random.Range(-jitter, jitter);
+
+
         }
 	}
 }
